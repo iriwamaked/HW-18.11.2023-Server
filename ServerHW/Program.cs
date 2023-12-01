795b6be7325e5803c89f02c709e760a5eb1e54fb
Irina Makedonska <iriwamaked@gmail.com>
2023-12-01 15:28:25 +0200

    01.12.23 15:27 - Working Server

--- ServerHW/Program.cs
+++ ServerHW/Program.cs
@@ -1,40 +1,60 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.Design;
+using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net.Sockets;
-using System.Collections.Concurrent;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Server
 {
     internal class Program
     {
-
-        private static ConcurrentDictionary<Socket, List<string>> clientsQuotes = new ConcurrentDictionary<Socket, List<string>>();
+        private static Dictionary<string, string> clientsIdentity;
+        private static ConcurrentDictionary<TcpClient, List<string>> clientsQuotes = new ConcurrentDictionary<TcpClient, List<string>>();
         private static readonly object lockObject = new object();
+        const int port = 8080;
+        static IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+        static TcpListener listener;
+        private static int currentClientCount = 0;
+        private static readonly object countLockObject = new object();
+        static int maxClientCount = 3;
 
         static async Task Main(string[] args)
         {
-            string ipAddress = "127.0.0.1";
-            int port = 8080;
-            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
-
-            using Socket listener = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            listener.Bind(ipEndPoint);
-            listener.Listen(10);
-
+            listener = new TcpListener(ipEndPoint);
+            listener.Start();
+            InitializeClients();
             Console.WriteLine("Сервер запущен!");
-
+            
             try
             {
                 while (true)
                 {
-                    Socket client = await listener.AcceptAsync();
-                    Console.WriteLine("Клиент присоединился");
-                    
-                    Task.Run(() => OneClientProcess(client));
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+
+                    if (await TakeClientCredentials(client))
+                    {
+                        if (CanAcceptClient())
+                        {
+                            Log($"Присоединился клиент: {client.Client.RemoteEndPoint}, {DateTime.Now}");
+                            await WriteMessageAsync(client, "Hello");
+
+                            IncrementClientCount();
+
+                            _ = OneClientProcess(client);
+                        }
+                        else
+                        {
+                            await WriteMessageAsync(client, "Later");
+                            Console.WriteLine("Достигнуто максимальное количество клиентов. Отклонено подключение нового клиента.");
+                            client.Close();
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -43,15 +63,100 @@ namespace Server
             }
         }
 
-        public static async Task OneClientProcess(Socket client)
+        private static void InitializeClients()
+        {
+            clientsIdentity = new Dictionary<string, string>()
+            {
+                {"user1", "password1" },
+                {"user2", "password2" },
+                {"user3", "password3" }
+            };
+        }
+
+        private static bool ValidateClient(string login, string pasword)
+        {
+            if (clientsIdentity.TryGetValue(login, out var currentPassword))
+            {
+                return pasword == currentPassword;
+            }
+            return false;
+        }
+
+        private static async Task< bool> TakeClientCredentials(TcpClient client)
         {
             try
             {
-                lock (lockObject)
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024]; 
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string clientCredentials = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+               //Cо STREAMREADER работать не хочет!
+                //using StreamReader reader = new StreamReader(stream);
+                //string clientCredentials = await reader.ReadLineAsync();
+                if (clientCredentials != null)
                 {
-                    clientsQuotes.TryAdd(client, new List<string>
+                    string[] credentialsArray = clientCredentials.Split(',');
+                    string login = credentialsArray[0];
+                    string password = credentialsArray[1];
+                    if (ValidateClient(login, password))
                     {
-                         "«Цели никогда не должны быть простыми. Они должны быть неудобными, чтобы заставить вас работать», — Майкл Фелпс",
+                        return true;
+                    }
+                    else
+                    {
+                        byte[] data = Encoding.Unicode.GetBytes("InvalidCredentials");
+                        await stream.WriteAsync(data, 0, data.Length);
+                        Log($"Ошибка подключения: Неверные учетные данные от клиента {client.Client.RemoteEndPoint}");
+                        client.Close();
+                        return false;
+                    }
+                }
+                else return false;
+                
+            }
+            catch (Exception ex)
+            {
+                // Обработка возможных исключений
+                Log($"Ошибка при обработке учетных данных клиента: {ex.Message}");
+                return false;
+            }
+            
+        }
+
+        // Метод для проверки, можно ли принять нового клиента
+        private static bool CanAcceptClient()
+        {
+            lock (countLockObject)
+            {
+                return currentClientCount < maxClientCount;
+            }
+        }
+
+        // Метод для увеличения счетчика активных клиентов
+        private static void IncrementClientCount()
+        {
+            lock (countLockObject)
+            {
+                currentClientCount++;
+            }
+        }
+
+        // Метод для уменьшения счетчика активных клиентов
+        private static void DecrementClientCount()
+        {
+            lock (countLockObject)
+            {
+                currentClientCount--;
+            }
+        }
+
+        public static void InitializeQuotes(TcpClient client)
+        {
+            lock (lockObject)
+            {
+                clientsQuotes.TryAdd(client, new List<string>
+                    {
+                        "«Цели никогда не должны быть простыми. Они должны быть неудобными, чтобы заставить вас работать», — Майкл Фелпс",
                 "«Возраст — это всего лишь ограничение, которое вы кладёте себе в голову», — Джеки Джойнер-Керси",
                 "«Не бойтесь неудач, потому что это ваш путь к успеху», — Леброн Джеймс",
                 "«Тело может многое выдержать. Вам нужно только убедить своё сознание в этом», — Эндрю Мерфи",
@@ -64,27 +169,44 @@ namespace Server
                 "«Нам не дано вернуть вчерашний день, но то, что будет завтра, зависит от нас», — Линдон Джонсон, 36-й президент США",
                 "«Никогда не поздно или — в моём случае — никогда не рано стать тем, кем ты хочешь стать. Временных рамок нет, можешь начать когда угодно. Можешь измениться или остаться прежним — правил не существует», — Фрэнсис Скотт Фицджеральд"
                     });
-                }
+            }
+        }
+        public static async Task OneClientProcess(TcpClient client)
+        {
+            NetworkStream stream = null;
+            try
+            {
+                stream = client.GetStream();
 
+
+                InitializeQuotes(client);
+
+                byte[] data = new byte[64];
                 while (client.Connected)
                 {
-                    byte[] buffer = new byte[1024];
-                    int bytesRead = await client.ReceiveAsync(buffer, SocketFlags.None);
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = await stream.ReadAsync(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
 
-                    if (bytesRead == 0)
-                        break;
-
-                    string message = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                    string message = builder.ToString();
                     Console.WriteLine($"Клиент: {message}");
 
                     if (message.ToLower() == "bye")
+                    {
+                        Log($"Клиент {client.Client.RemoteEndPoint} отключился в: {DateTime.Now}");
+                        DecrementClientCount();
                         break;
+                    }
 
                     string response = GetComputerResponse(client);
-                    byte[] data = Encoding.Unicode.GetBytes(response);
-                    await client.SendAsync(data, 0);
+                    data = Encoding.Unicode.GetBytes(response);
+                    await stream.WriteAsync(data, 0, data.Length);
 
-                    Console.WriteLine($"Ответ сервера: {response}");
+                    Log($"Цитата: {response}");
 
                     if (response == "bye")
                     {
@@ -92,13 +214,20 @@ namespace Server
                         {
                             clientsQuotes.TryRemove(client, out _);
                         }
-
-                        client.Shutdown(SocketShutdown.Both);
-                        client.Close();
                         Console.WriteLine("Соединение разорвано сервером из-за окончания цитат и направления сообщения \"bye\"");
+                        Log($"Клиент {client.Client.RemoteEndPoint} отключился в: {DateTime.Now}");
+                        DecrementClientCount();
+                        if (stream != null)
+                            client.Close();
+                        if (client != null)
+                            client.Close();
+
                         break;
                     }
                 }
+
+
+
             }
             catch (ObjectDisposedException)
             {
@@ -117,13 +246,12 @@ namespace Server
 
                 if (client != null && client.Connected)
                 {
-                    client.Shutdown(SocketShutdown.Both);
                     client.Close();
                 }
             }
         }
 
-        public static string GetComputerResponse(Socket client)
+        public static string GetComputerResponse(TcpClient client)
         {
             lock (lockObject)
             {
@@ -141,6 +269,21 @@ namespace Server
                     return "bye";
                 }
             }
+        }
+
+        public static void Log(string message)
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine(message);
+                // логируйте в файл или другие места по необходимости
+            }
+        }
+        public static async Task WriteMessageAsync(TcpClient client, string message)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            await stream.WriteAsync(data, 0, data.Length);
         }
     }
 }
